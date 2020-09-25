102bbfb9fe7e9cc118a88432437369c12626f964
yang <anyang47@hotmail.com>
2020-09-25 19:48:56 -0400

    change3

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.use(
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+  console.log(`server IS running on port ${PORT}`);
 });
